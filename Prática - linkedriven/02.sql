571b10931ad71978950180ca640670fd8a064625
Renato da Silva Viana <verkylen@gmail.com>
2022-12-15 03:42:01 -0300

    Linkedriven practice done

---
+++ Prática - linkedriven/02.sql
@@ -0,0 +1,2 @@
+SELECT testimonials.id, w.name AS writer, r.name AS recipient, testimonials.message FROM testimonials JOIN users w ON testimonials."writerId" = w.id JOIN users r ON 
+testimonials."recipientId" = r.id ORDER BY testimonials.id;
