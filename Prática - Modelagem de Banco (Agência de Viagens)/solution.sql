8fcd461e9c0686735e94264bf700741a96fccb51
Renato da Silva Viana <verkylen@gmail.com>
2022-12-27 01:05:17 -0300

    Travels agency practice solved

--- Prática - Modelagem de Banco (Agência de Viagens)/solution.sql
+++ Prática - Modelagem de Banco (Agência de Viagens)/solution.sql
@@ -14,15 +14,14 @@ CREATE TABLE flights (
     "arrivalTime" TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-ALTER TABLE flights ADD CONSTRAINT flights_fk0 FOREIGN KEY "airlineId" REFERENCES airlines(id);
-
-ALTER TABLE flights ADD CONSTRAINT flights_fk1 FOREIGN KEY "departureAirportId" REFERENCES airports(id);
-
-ALTER TABLE flights ADD CONSTRAINT flights_fk2 FOREIGN KEY "arrivalAirportId" REFERENCES airports(id);
-
 CREATE TABLE airports (
     id SERIAL PRIMARY KEY,
     acronym TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL UNIQUE,
-
+    name TEXT NOT NULL UNIQUE
 );
+
+ALTER TABLE flights ADD CONSTRAINT flights_fk0 FOREIGN KEY ("airlineId") REFERENCES airlines(id);
+
+ALTER TABLE flights ADD CONSTRAINT flights_fk1 FOREIGN KEY ("departureAirportId") REFERENCES airports(id);
+
+ALTER TABLE flights ADD CONSTRAINT flights_fk2 FOREIGN KEY ("arrivalAirportId") REFERENCES airports(id);
